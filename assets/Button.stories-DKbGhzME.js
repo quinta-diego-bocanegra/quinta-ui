6f74c8f67b0446c185bbbff6728d8254ad5e1a72
SakugaCoder <SakugaCoder@users.noreply.github.com>
2024-12-23 02:27:01 +0000

    deploy: 7ecf4a6cceda1c40e4015d8a3fc70db4710ae85d

---
+++ assets/Button.stories-DKbGhzME.js
@@ -0,0 +1,25 @@
+import{f as z}from"./index-DQLiMaGX.js";import{B as x}from"./Button-BgKzvVr-.js";import"./jsx-runtime-WdPq8kIh.js";import"./index-BpYrhlGc.js";const E={title:"Example/Button",component:x,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:z()}},r={args:{primary:!0,label:"Button"}},e={args:{label:"Button"}},a={args:{outlined:!0,label:"Button"}},o={args:{size:"large",label:"Button"}},s={args:{size:"small",label:"Button"}};var t,n,l;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: 'Button'
+  }
+}`,...(l=(n=r.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};var c,u,m;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    label: 'Button'
+  }
+}`,...(m=(u=e.parameters)==null?void 0:u.docs)==null?void 0:m.source}}};var p,i,d;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    outlined: true,
+    label: 'Button'
+  }
+}`,...(d=(i=a.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};var g,B,b;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    size: 'large',
+    label: 'Button'
+  }
+}`,...(b=(B=o.parameters)==null?void 0:B.docs)==null?void 0:b.source}}};var S,y,f;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    label: 'Button'
+  }
+}`,...(f=(y=s.parameters)==null?void 0:y.docs)==null?void 0:f.source}}};const L=["Primary","Secondary","Outlined","Large","Small"];export{o as Large,a as Outlined,r as Primary,e as Secondary,s as Small,L as __namedExportsOrder,E as default};
